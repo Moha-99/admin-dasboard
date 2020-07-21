6a0eac30c45038896cab7d50bd214ecc699d518c
Moha_99 <master27599@gmail.com>
2020-07-21 21:33:19 +0200

    Updates

---
+++ precache-manifest.a9d1676cfa081dfdecb2b53520233e93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15f737d8f3ccedf748f706b112773e1e",
+    "url": "/admin-dasboard/index.html"
+  },
+  {
+    "revision": "c4c5ff5b72cb236896ae",
+    "url": "/admin-dasboard/static/css/2.37865b2a.chunk.css"
+  },
+  {
+    "revision": "5af19d5becfe9503ad57",
+    "url": "/admin-dasboard/static/css/main.2de9dc2c.chunk.css"
+  },
+  {
+    "revision": "c4c5ff5b72cb236896ae",
+    "url": "/admin-dasboard/static/js/2.f5f51875.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/admin-dasboard/static/js/2.f5f51875.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5af19d5becfe9503ad57",
+    "url": "/admin-dasboard/static/js/main.1bf9416a.chunk.js"
+  },
+  {
+    "revision": "b64c72a4dba6cb599ce4",
+    "url": "/admin-dasboard/static/js/runtime-main.e4afc91d.js"
+  }
+]);
